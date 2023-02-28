4ebcad31cd6f0711204d87968fcc21c8e80e4300
BrunoMiguelTeixeira <migamen1000@gmail.com>
2023-02-28 22:22:54 +0000

    dtStat function updated

--- dailytemp.c
+++ dailytemp.c
@@ -6,3 +6,26 @@ int main(void){
 
 	return 0;
 }
+
+
+void dtStat(int* array,int* temps){
+	static int i=0;
+	static int sum=0;
+	int array_size=*(&array +1)-array;
+	sum=sum+array[i];
+	if(i==0){
+		temps[0]=array[i];
+		temps[1]=array[i];
+	}
+	else{
+		if(array[i]>temps[0])
+			temps[0]=array[i];
+		if(array[i]<temps[1])
+			temps[1]=array[i];
+	}
+	if(array_size==i)
+		i=0;
+	i++;
+	temps[2]=sum/i;
+}
+
