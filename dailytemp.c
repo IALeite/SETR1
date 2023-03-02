78e652c4a76047f22814fad143a19f51ab7bd6a2
IALeite <ifaleite@ua.pt>
2023-03-02 13:12:56 +0000

    main and vector init

--- dailytemp.c
+++ dailytemp.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+/** Inicializes a vector to store tempratures during a day*/
+void dtInit(int* dayTemp);
+
+
 
 int main(void){
-	
 
+	int values[3];
+	int dTemp[24];
+	
+	dtInit(dTemp);
+	
+	for(int i = 0; i<24; i++)
+	{
+		dtAdd(&dTemp);
+		dtStat(&dTemp, &values);
+		printf("Máx: %d Min: %d Average: %d \n",values[0], values[1], values[2]); /// Display current stats values	
+	}
+	
 	return 0;
 }
+
+
+void dtInit(int* dTemp)
+{
+	for(int i = 0 ; i<24; i++)
+	{
+		dTemp[i] = 100;
+	}
+
+}
+
+
+
